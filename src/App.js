6fd0920c2f25dbc249d2bf29820b0d37ecef5539
Thushara Thiwanka <thusharathiwanka01@gmail.com>
2021-11-27 03:48:41 +0530

    Footer Added

--- src/App.js
+++ src/App.js
@@ -8,6 +8,13 @@ function App() {
 	return (
 		<>
 			<AllData />
+
+			{/* <!--[ footer ] --> */}
+			<div id="footer">
+				<div class="container">
+					<p class="footer-block">Project by <a href="https://github.com/ThusharaX" target="_blank" rel="noopener noreferrer">Thushara Thiwanka</a></p>
+				</div>
+			</div>
 		</>
 	)
 }
