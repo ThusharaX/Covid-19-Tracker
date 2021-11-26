86ae1a327a3a0cf3341bfad00f136d3c251a4354
Thushara Thiwanka <thusharathiwanka01@gmail.com>
2021-11-27 03:31:24 +0530

    Update AllData.js

--- src/AllData.js
+++ src/AllData.js
@@ -73,7 +73,7 @@ function AllData() {
                                                 }
                                                 <div className="card-body">
                                                     <h5 className="card-title">{country.country}</h5>
-                                                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                                    <p className="card-text"></p>
                                                 </div>
                                                 <ul className="list-group list-group-flush">
                                                     <li className="list-group-item">Cases: {country.cases}</li>
